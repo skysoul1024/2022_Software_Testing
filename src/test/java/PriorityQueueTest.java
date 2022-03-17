b31f743e0cf4f8841df80db01f77abb9b074ef10
skysoul1024 <80204490+skysoul1024@users.noreply.github.com>
2022-03-17 13:23:06 +0800

    Update PriorityQueueTest.java

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,8 @@ class PriorityQueueTest {
                 Arguments.of(new int[]{3, 1, 2}, new int[]{1, 2, 3}),
                 Arguments.of(new int[]{-3, -1, -2, 5}, new int[]{-3, -2, -1, 5}),
                 Arguments.of(new int[]{5, 4, 3, 2, 1}, new int[]{1, 2, 3, 4, 5}),
-                Arguments.of(new int[]{0, 0, 0}, new int[]{0, 0, 0})
+                Arguments.of(new int[]{0, 0, 0}, new int[]{0, 0, 0}),
+                Arguments.of(new int[]{3, 2, 1}, new int[]{1, 2 ,3})
         );
     }
 
